4998083bad3eb86ba72c3e1bc57b76f3703db6d3
Eric Crosson <eric.s.crosson@utexas.edu>
2023-06-13 20:44:35 -0500

    build(nix): drop dependency on flake-parts

--- flake.nix
+++ flake.nix
@@ -3,83 +3,76 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
     pre-commit-hooks = {
       url = "github:cachix/pre-commit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs @ {
+  outputs = {
     self,
-    flake-parts,
     nixpkgs,
     pre-commit-hooks,
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = [
-        "aarch64-darwin"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ];
+  }: let
+    forEachSystem = nixpkgs.lib.genAttrs [
+      "aarch64-darwin"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+  in {
+    formatter = forEachSystem (system: nixpkgs.legacyPackages.${system}.alejandra);
 
-      perSystem = {
-        pkgs,
-        system,
-        ...
-      }: let
-        tex = pkgs.texlive.combine {
-          inherit
-            (pkgs.texlive)
-            scheme-small
-            # LaTeX packages here
-            
-            enumitem
-            hyperref
-            paracol
-            # build tools
-            
-            latexmk
-            ;
-        };
-      in rec {
-        formatter = pkgs.alejandra;
-
-        checks = {
-          pre-commit-check = pre-commit-hooks.lib.${system}.run {
-            src = ./.;
-            hooks = {
-              actionlint.enable = true;
-              alejandra.enable = true;
-              prettier.enable = true;
-              statix.enable = true;
-            };
-          };
-        };
-
-        devShells.default = nixpkgs.legacyPackages.${system}.mkShell {
-          shellHook = ''
-            ${self.checks.${system}.pre-commit-check.shellHook}
-          '';
-        };
-
-        packages = {
-          default = packages.document;
-          document = pkgs.stdenvNoCC.mkDerivation {
-            name = "Eric_Crosson_Resume";
-            src = self;
-            phases = ["unpackPhase" "buildPhase" "installPhase"];
-            buildInputs = [pkgs.coreutils pkgs.glibcLocales tex];
-            buildPhase = "make";
-            installPhase = ''
-              mkdir -p $out
-              cp Eric_Crosson_Resume.pdf $out/
-            '';
-          };
+    checks = forEachSystem (system: let
+      pre-commit-check = pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          actionlint.enable = true;
+          alejandra.enable = true;
+          prettier.enable = true;
+          statix.enable = true;
         };
       };
-    };
+    in {
+      inherit pre-commit-check;
+    });
+
+    devShells = forEachSystem (system: {
+      default = nixpkgs.legacyPackages.${system}.mkShell {
+        shellHook = ''
+          ${self.checks.${system}.pre-commit-check.shellHook}
+        '';
+      };
+    });
+
+    packages = forEachSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      tex = pkgs.texlive.combine {
+        inherit
+          (pkgs.texlive)
+          scheme-small
+          # LaTeX packages here
+          
+          enumitem
+          hyperref
+          paracol
+          # build tools
+          
+          latexmk
+          ;
+      };
+    in {
+      default = self.packages.${system}.document;
+      document = pkgs.stdenvNoCC.mkDerivation {
+        name = "Eric_Crosson_Resume";
+        src = self;
+        phases = ["unpackPhase" "buildPhase" "installPhase"];
+        buildInputs = [pkgs.coreutils pkgs.glibcLocales tex];
+        buildPhase = "make";
+        installPhase = ''
+          mkdir -p $out
+          cp Eric_Crosson_Resume.pdf $out/
+        '';
+      };
+    });
+  };
 }
